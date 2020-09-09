01606d4b0e81a27bd0dee71c68cdf28200d799bc
pascal.desprez <pascal.desprez@fr.ibm.com>
2020-09-09 23:58:32 +0200

    import job jpa implementation

---
+++ src/main/java/com/springbatchdemo/Book.java
@@ -0,0 +1,87 @@
+package com.springbatchdemo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String title;
+
+	private String author;
+
+	private String isbn;
+
+	private String publisher;
+
+	private Integer year;
+
+	// Constructors, Getters and Setters
+
+	public Book() {
+		super();
+	}
+
+	public Book(final String title, final String author, final String isbn, final String publisher,
+			final Integer year) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.publisher = publisher;
+		this.year = year;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(final String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(final String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(final String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(final Integer year) {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return "BookDto [title=" + title + ", author=" + author + ", isbn=" + isbn + ", publisher=" + publisher
+				+ ", year=" + year + "]";
+	}
+
+}
--- src/main/java/com/springbatchdemo/ImportJobConfig.java
+++ src/main/java/com/springbatchdemo/ImportJobConfig.java
@@ -1,6 +1,6 @@
 package com.springbatchdemo;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManagerFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +10,7 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -33,7 +32,7 @@ public class ImportJobConfig {
 	private StepBuilderFactory stepBuilderFactory;
 
 	@Autowired
-	private DataSource dataSource;
+	private EntityManagerFactory entityManagerFactory;
 
 	@Bean
 	public Job importJob() {
@@ -46,18 +45,20 @@ public class ImportJobConfig {
 	@Bean
 	public Step importStep() {
 		return stepBuilderFactory.get("import-step") //
-				.<BookDto, BookDto>chunk(5) //
+				.<BookDto, Book>chunk(5) //
 				.reader(reader()) //
 				.processor(processor()).writer(writer()) //
 				.build();
 	}
 
-	private ItemProcessor<BookDto, BookDto> processor() {
-		return new ItemProcessor<BookDto, BookDto>() {
+	private ItemProcessor<BookDto, Book> processor() {
+		return new ItemProcessor<BookDto, Book>() {
 
-			public BookDto process(final BookDto item) throws Exception {
+			public Book process(final BookDto item) throws Exception {
 				LOGGER.info(item.toString());
-				return item;
+				final Book book = new Book(item.getTitle(), item.getAuthor(), item.getIsbn(), item.getPublisher(),
+						item.getPublishedOn());
+				return book;
 			}
 		};
 	}
@@ -83,12 +84,9 @@ public class ImportJobConfig {
 	}
 
 	@Bean
-	public JdbcBatchItemWriter<BookDto> writer() {
-		final JdbcBatchItemWriter<BookDto> writer = new JdbcBatchItemWriter<BookDto>();
-		writer.setDataSource(dataSource);
-		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<BookDto>());
-		writer.setSql(
-				"INSERT INTO book(title, author, isbn, publisher, year) VALUES (:title, :author, :isbn, :publisher, :publishedOn )");
+	public JpaItemWriter<Book> writer() {
+		final JpaItemWriter<Book> writer = new JpaItemWriter<Book>();
+		writer.setEntityManagerFactory(entityManagerFactory);
 		return writer;
 	}
 }
